5a29b27248920fb0842e4e44036f23e8e164cb6a
Desenvolvedor <desenvolvedor@MBP-de-Bruno.home>
2018-04-22 18:43:29 -0300

    Adding validation to inputs

--- assets/scripts/login.ctrl.js
+++ assets/scripts/login.ctrl.js
@@ -1,35 +1,64 @@
 (function () {
     "use strict";
+    let passwordValid = true;
 
 
     // Event Listener Here
     document.getElementById('btnSubmit').addEventListener('click', function () {
-        // validate data from input
-        // send data to server
-        
-
+        let userLogin = getUserLoginData();
+        if(!userLogin) {
+            console.log('nao podemos enviar pro servidor')
+        }
     });
 
-    document.getElementById('password').addEventListener('keyup', function (event) {
-        // verify if there value is valid
-        if(event.target.value.length !== 6) {
-            validateField(event.target);
-        }
-    })
+    // document.getElementById('password').addEventListener('keyup', function (event) {
+        
+    // })
 
     // Functions here
-    function validatePassword() {
-        let pass = document.getElementById('password');
+    function getUserLoginData() {
 
-        if(pass.value.length < 6) {
-            passwordLengthErro(pass)
+        let isValid;
+
+        const userLogin = {
+            email: document.getElementById('user-email').value,
+            password: document.getElementById('user-password').value
         }
+
+        if(userLogin.email === '') {
+            console.log('email')
+            isValid = invalidField('user-email');
+        }
+
+        if (userLogin.password === '') {
+            console.log('senha')
+            isValid = invalidField('user-password');
+        }
+        return isValid ? userLogin : false;
     }
 
-    function passwordLengthErro(field) {
+    function invalidField(inputId) {
+        let inputName;
+        let msgContainer;
 
+        if(inputId === 'user-email') {
+            msgContainer = document.getElementById('label-user')
+            inputName = 'Email'
+        } else if(inputId === 'user-password') {
+            msgContainer = document.getElementById('label-password')
+            inputName = 'Senha'
+        }
+        const template = `
+            <p class="error-msg" id="error-${inputName}">
+                O campo ${inputName} precisa ser preencido
+            </p>
+        `;
+        if(document.getElementById(`error-${inputName}`)) {
+            return false;
+        }
+
+        msgContainer.innerHTML += template;
+        return false;
     }
-
-
 
 })();
