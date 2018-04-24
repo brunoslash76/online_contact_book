3bb3646ea8f33d68dde8470ac046a30170307d24
Desenvolvedor <desenvolvedor@MBP-de-Bruno.home>
2018-04-23 23:17:07 -0300

    Adding requisition to a server... first time playing with HTTP request in pure AJAX
    - Whats gonna be?
    - How can I asure myself?
    - God help me!!
    - Need to create an API ... Damn...

--- assets/scripts/login2.js
+++ assets/scripts/login2.js
@@ -10,12 +10,27 @@
 
     // Listeners
     btnSend.addEventListener('click', function () {
-        let userInfoLogin = {
-            email: setEmail(inputEmail),
-            password: setPassword(inputPassword)
+        let email = setEmail(inputEmail);
+        let password = setPassword(inputPassword);
+        let userInfoLogin = undefined;
+
+        if (email && password) {
+            userInfoLogin = {
+                email: email,
+                password: password
+            }
         }
+        if (userInfoLogin) {    
+            login(userInfoLogin);
+        }
+
         
     })
+
+
+
+
+
 
     // Functions
     function setEmail(email) {
@@ -23,11 +38,12 @@
             // setError for email input
             setError(email);
             addErrorMessage(email);
-            return;
+            return undefined;
         }
         if(email.classList.contains('error')) {
             removeError(email)
             removeErrorMessage(email)
+            console.log(email.value);
             return email.value;
         }
     }
@@ -36,21 +52,20 @@
         if(!password.value || password.value === '') {
             setError(password);
             addErrorMessage(password);
-            return;
+            return undefined;
         }
 
         if(password.classList.contains('error')) {
             removeError(password)
             removeErrorMessage(password)
+            console.log(password.value)
             return password.value;
         }
     }
 
     // other methods
     function setError(input){
-        console.log(input.getAttribute('name'))
         if (input.classList.contains('error')) {
-            console.log('funcionou');
             return;
         }
         input.className += ' error';
@@ -76,6 +91,28 @@
         input.setAttribute('valid', 'true')
         let pe = input.parentElement;
         pe.removeChild(pe.childNodes[4])
+    }
+
+    function login(user) {
+        /**
+         * @todo criar api básica para receber esse requisition
+         */
+        console.log(user)
+        let req = new XMLHttpRequest();
+        req.open('POST', 'user/login');
+        req.setRequestHeader(
+            'ContentType',
+            'application/json'
+        );
+        req.onload = function (e) {
+            console.log(e);
+            if (req.status === 200) {
+                console.log('requisição para o servidor OK...');
+                let userInfo = JSON.parse(req.responseText);
+                console.log(userInfo);
+            }
+        }
+        req.send(JSON.stringify(user));
     }
 
 
