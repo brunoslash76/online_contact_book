c9fb44f595098a4e643372da28b47018c1ecbb59
Desenvolvedor <desenvolvedor@MBP-de-Bruno.home>
2018-04-23 23:02:26 -0300

    Fixing some bug in the code
    - The error message do not repeat as the button send is clicked
    - The class error do not repeat as the button send is clicked

--- assets/scripts/login2.js
+++ assets/scripts/login2.js
@@ -6,6 +6,8 @@
     let inputPassword = document.getElementById('user-password');
     let btnSend = document.getElementById('btnSubmit');
 
+
+
     // Listeners
     btnSend.addEventListener('click', function () {
         let userInfoLogin = {
@@ -25,6 +27,7 @@
         }
         if(email.classList.contains('error')) {
             removeError(email)
+            removeErrorMessage(email)
             return email.value;
         }
     }
@@ -38,13 +41,18 @@
 
         if(password.classList.contains('error')) {
             removeError(password)
+            removeErrorMessage(password)
             return password.value;
         }
-
     }
 
     // other methods
     function setError(input){
+        console.log(input.getAttribute('name'))
+        if (input.classList.contains('error')) {
+            console.log('funcionou');
+            return;
+        }
         input.className += ' error';
     }
 
@@ -53,15 +61,22 @@
     }
 
     function addErrorMessage(input) {
+        if(input.getAttribute('valid') === 'false') {
+            return;
+        }
+        input.setAttribute('valid', 'false')
         let errorMessage = `O campo ${input.getAttribute('name')} está inválido!`;
         let paragraph = document.createElement('p');
-        paragraph.setAttribute('contentEditable', 'true');
-        paragraph.setAttribute('class', 'error-msg')
+        paragraph.setAttribute('class', 'error-msg');
         paragraph.innerHTML = errorMessage;
-        input.insertAdjacentElement('afterend', paragraph   )
+        input.insertAdjacentElement('afterend', paragraph);
     }
 
-
+    function removeErrorMessage(input) {
+        input.setAttribute('valid', 'true')
+        let pe = input.parentElement;
+        pe.removeChild(pe.childNodes[4])
+    }
 
 
 
